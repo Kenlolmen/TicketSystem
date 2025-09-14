094a0da5c1905801762a0654277e038b0a8acb0e
kenlolmen <ojjaruga@gmail.com>
2025-09-14 22:03:21 +0200

    Commands & Handlers

--- src/TicketSystem.Core/Repositories/ITicketRepository.cs
+++ src/TicketSystem.Core/Repositories/ITicketRepository.cs
@@ -8,6 +8,7 @@ namespace TicketSystem.Core.Repositories
     public interface ITicketRepository
     {
         Task<Ticket?> GetByIdAsync(Guid id);
+        Task<IEnumerable<Ticket>> GetByStatus(Status status);
         Task AddAsync(Ticket ticket);
         Task UpdateAsync(Ticket ticket);
         Task DeleteAsync(Guid id);
